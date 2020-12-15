5a474fc7fa13ba89df132bb4d38fa168285e1356
kisum1024 <43131669+kisum1024@users.noreply.github.com>
2020-12-15 22:00:28 +0900

    first
    처음올리네. 어제했어야하는데 이걸안했네
    내일은 콤보 마우스 누를때만 진행되도록 해야하는데 큰일이다.

---
+++ Assets/2.Script/ElementalArtist.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class ElementalArtist : MonoBehaviour
+{
+
+    private static CharacterController characterController;
+    private static Animator animator;
+
+    public float speed = 5f;
+    public float gravity = 9.8f;
+    public float jumpspeed = 10f;
+    public float rotSpeed = 5f;
+    public Vector3 dir;
+    public float laydistance = 40;
+    bool isground = false;
+
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, laydistance))
+        {
+            isground = true;
+        }
+        else
+        {
+            isground = false;
+        }
+        float mouseX = Input.GetAxis("Mouse X");
+        if(Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("EnterCombo");
+            Debug.Log("시작");
+        }
+
+        
+        transform.Rotate(Vector3.up * rotSpeed * mouseX);
+        if (isground)
+        {
+            
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            dir = new Vector3(x, 0, z);
+
+            dir = transform.TransformDirection(dir);
+
+            dir = dir * speed;
+            
+            if(Input.GetButton("Jump"))
+            {
+                dir.y = jumpspeed;
+            }
+
+        }
+
+        dir.y -= gravity * Time.deltaTime;
+        characterController.Move(dir * Time.deltaTime);
+
+        animator.SetFloat("MoveX", dir.x);
+        animator.SetFloat("MoveZ", dir.z);
+        animator.SetFloat("SpeedY", dir.y);
+        animator.SetBool("IsGround", characterController.isGrounded);
+        
+
+        
+
+        
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, Vector3.forward * laydistance);
+
+    }
+
+
+}
